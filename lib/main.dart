41ed5b9bd46f4b85b1fe520f30f1468483ff97e1
PavloShemelov <pavel.shemelov.ua@gmail.com>
2024-04-09 18:19:57 +0300

    Added a home screen

--- lib/main.dart
+++ lib/main.dart
@@ -153,26 +153,41 @@ class MapWithHeaderAndFooter extends StatelessWidget {
             ),
           ),
           Positioned(
-            left: 0,
-            right: 0,
-            bottom: 120, // Отступ между footer и карточками
+            left: 0, // Отступ слева
+            right: 0, // Отступ справа
+            bottom: 120, // Отступ между футером и карточками
             child: SizedBox(
               height: 140, // Высота карточек
               child: ListView.builder(
                 scrollDirection: Axis.horizontal,
                 itemCount: 3, // Количество карточек
                 itemBuilder: (BuildContext context, int index) {
+                  Color slotColor;
+                  if (index == 0) {
+                    slotColor = Color.fromRGBO(255, 215, 2, 0.7); // Цвет для первой карточки
+                  } else if (index == 1) {
+                    slotColor = Colors.green; // Цвет для второй карточки
+                  } else {
+                    slotColor = Colors.red; // Цвет для третьей карточки
+                  }
                   return Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: CardWidget(),
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0), // Отступы между карточками
+                    child: CardWidget(
+                      title: index == 0 ? 'Ülemiste Keskus' : index == 1 ? 'Mercury Hotell' : 'Circle K Järvevana',
+                      address: index == 0 ? 'Suur sõjamäe 3' : index == 1 ? 'Lennujaam 3' : 'Järvevana tee2',
+                      slots: index == 0 ? '3 free slots left' : index == 1 ? '10 free slots left' : '1 free slot left',
+                      slotColor: slotColor, // Передача цвета в CardWidget
+                      time: index == 0 ? '08:00 - 23:00' : index == 1 ? '09:00 - 21:00' : '08:00 - 00:00',
+                    ),
                   );
                 },
               ),
             ),
           ),
+
           Positioned(
             bottom: 280, // Расположение кнопки навигации
-            left: 330,
+            left: 310,
             child: Container(
               decoration: BoxDecoration(
                 color: Colors.white, // ваш цвет фона
@@ -200,7 +215,6 @@ class MapWithHeaderAndFooter extends StatelessWidget {
   }
 }
 
-
 class Header extends StatelessWidget {
   const Header({Key? key}) : super(key: key);
 
@@ -211,37 +225,37 @@ class Header extends StatelessWidget {
       children: [
         Container(
           decoration: BoxDecoration(
-            color: Colors.white, // ваш цвет фона
-            borderRadius: BorderRadius.circular(40), // если нужно закруглить углы
+            color: Colors.white, // Background color
+            borderRadius: BorderRadius.circular(40), // If you want to round the corners
           ),
           child: IconButton(
             onPressed: () {
               // Add functionality for the menu button here
             },
             icon: Icon(Icons.menu),
-            color: Colors.black, // Цвет иконки
-            iconSize: 24, // Размер иконки
+            color: Colors.black, // Icon color
+            iconSize: 24, // Icon size
           ),
         ),
         ElevatedButton.icon(
           onPressed: () {
             // Add functionality for the search button here
           },
-          icon: Icon(Icons.search), // Иконка слева от текста
-          label: Text('Search Parking Slot'),
+          icon: Icon(Icons.search), // Icon on the left of the text
+          label: const Text('Search Parking Slot'),
         ),
         Container(
           decoration: BoxDecoration(
-            color: Colors.white, // ваш цвет фона
-            borderRadius: BorderRadius.circular(40), // если нужно закруглить углы
+            color: Colors.white, // Background color
+            borderRadius: BorderRadius.circular(40), // If you want to round the corners
           ),
           child: IconButton(
             onPressed: () {
               // Add functionality for the menu button here
             },
             icon: Icon(Icons.filter_list_alt),
-            color: Colors.black, // Цвет иконки
-            iconSize: 24, // Размер иконки
+            color: Colors.black, // Icon color
+            iconSize: 24, // Icon size
           ),
         ),
       ],
@@ -250,22 +264,67 @@ class Header extends StatelessWidget {
 }
 
 class CardWidget extends StatelessWidget {
-  const CardWidget({Key? key}) : super(key: key);
+  final String title;
+  final String address;
+  final String slots;
+  final String time;
+  final Color slotColor; // Новый параметр для цвета текстовой кнопки
+
+  const CardWidget({
+    Key? key,
+    required this.title,
+    required this.address,
+    required this.slotColor,
+    required this.slots,
+    required this.time,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 224,
       height: 140,
+      padding: const EdgeInsets.all(8.0), // Добавим отступы внутри контейнера
       decoration: BoxDecoration(
         color: Colors.grey[300],
         borderRadius: BorderRadius.circular(10),
       ),
-      child: Center(
-        child: Text(
-          'Your Card',
-          style: TextStyle(fontSize: 20),
-        ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start, // Выравнивание по левому краю
+        children: [
+          Text(
+            title,
+            style: const TextStyle(fontSize: 16),
+          ),
+          const SizedBox(height: 4), // Добавим небольшой отступ
+          Text(
+            address,
+            style: const TextStyle(fontSize: 10),
+          ),
+          const SizedBox(height: 4), // Добавим небольшой отступ
+          ElevatedButton(
+            onPressed: () {}, // Ваша функция обработки нажатия
+            style: ElevatedButton.styleFrom(
+              primary: slotColor, // Цвет кнопки
+              padding: const EdgeInsets.all(5), // Отступы кнопки
+            ),
+            child: Text(
+              slots,
+              style: const TextStyle(color: Colors.black), // Цвет текста кнопки
+            ), // Текст кнопки
+          ),
+          const SizedBox(height: 4), // Добавим небольшой отступ
+          Row(
+            children: [
+              const Icon(Icons.access_time),
+              const SizedBox(width: 5),
+              Text(
+                time,
+                style: const TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
